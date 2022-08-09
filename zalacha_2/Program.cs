b42181abe4053b96a3a612958bc078d5b15f1fa1
Mike <ma95gal@mail.ru>
2022-08-10 00:53:30 +0300

    Написали задачу №2

---
+++ zalacha_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число:");
+int namberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int namberB = Convert.ToInt32(Console.ReadLine());
+
+if (namberA > namberB)
+{
+    Console.WriteLine("Максимальное число равно");
+    Console.WriteLine(namberA);
+}
+else
+{
+    Console.WriteLine("Максимальное число равно");
+    Console.WriteLine(namberB);
+}
